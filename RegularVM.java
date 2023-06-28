16118cfad82a604a78213388a45c6402659bdbce
CarandangR <133246912+CarandangR@users.noreply.github.com>
2023-06-28 15:46:26 +0800

    created RegularVM file

---
+++ RegularVM.java
@@ -0,0 +1,16 @@
+public class RegularVM
+{
+    Slots[] itemSlots;
+    Money vendBalance;
+    Money userBalance;
+    String vendName;
+    int slotCapacity;
+    int itemCapacity;
+
+    public RegularVM(String vendName, int slotCapacity, int itemCapacity)
+    {
+        this.slotCapacity = slotCapacity;
+        this.vendName = vendName;
+        itemSlots
+    }
+}
