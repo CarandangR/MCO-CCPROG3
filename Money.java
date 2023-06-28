28d502a7f7ae5dc0193c8eb2953aacbdd25a07d7
CarandangR <133246912+CarandangR@users.noreply.github.com>
2023-06-28 14:13:51 +0800

    Money Class finished

---
+++ Money.java
@@ -0,0 +1,116 @@
+public class Money
+{
+    int coin1;
+    int coin5;
+    int coin10;
+    int coin20;
+    int bill50;
+    int bill100;
+    int bill200;
+    int bill500;
+    int bill1000;
+    int totalMoney;
+
+    public int getCoin1()
+    {
+        return coin1;
+    }
+
+    public void setCoin1(int coin1)
+    {
+        this.coin1 = coin1;
+    }
+
+    public int getCoin5()
+    {
+        return coin5;
+    }
+
+    public void setCoin5(int coin5)
+    {
+        this.coin5 = coin5;
+    }
+
+    public int getCoin10()
+    {
+        return coin10;
+    }
+
+    public void setCoin10(int coin10)
+    {
+        this.coin10 = coin10;
+    }
+
+    public int getCoin20()
+    {
+        return coin20;
+    }
+
+    public void setCoin20(int coin20)
+    {
+        this.coin20 = coin20;
+    }
+
+    public int getBill50()
+    {
+        return bill50;
+    }
+
+    public void setBill50(int bill50)
+    {
+        this.bill50 = bill50;
+    }
+
+    public int getBill100()
+    {
+        return bill100;
+    }
+
+    public void setBill100(int bill100)
+    {
+        this.bill100 = bill100;
+    }
+
+    public int getBill200()
+    {
+        return bill200;
+    }
+
+    public void setBill200(int bill200)
+    {
+        this.bill200 = bill200;
+    }
+
+    public int getBill500()
+    {
+        return bill500;
+    }
+
+    public void setBill500(int bill500)
+    {
+        this.bill500 = bill500;
+    }
+
+    public int getBill1000()
+    {
+        return bill1000;
+    }
+
+    public void setBill1000(int bill1000)
+    {
+        this.bill1000 = bill1000;
+    }
+
+    public void getTotalMoney()
+    {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(int totalMoney)
+    {
+        this.totalMoney = totalMoney;
+    }
+
+    public Money()
+    {}
+}
